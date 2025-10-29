caf64a171970b472d8c1dfcc23cb83ac37f6aa0f
snipeet03 <navneetlonare77@gmail.com>
2025-10-29 20:49:01 +0530

    Initialize context API project with Vite and React setup, including basic styling and Todo context implementation.

---
+++ intermediate/contextApi/src/context/TodoContext.js
@@ -0,0 +1,26 @@
+import React , { createContext , useContext} from "react";
+
+
+export const TodoContext =  createContext({
+    todos : [
+        {
+            id : 1, 
+            todo :"My todo ", 
+            completed : false 
+        }, 
+    ], 
+
+    addTdodo : (todo) => {}, 
+    updateTodo : (id, todo) => {}, 
+    deleteTodo : (id) => {},
+    toggleCompleted : (id) => {}, 
+
+})
+
+
+export const useTodo = () => {
+    return useContext(TodoContext);
+}
+
+
+export const TodoProvider =  TodoContext.Provider;
