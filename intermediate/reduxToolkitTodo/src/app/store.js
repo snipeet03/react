bf5ddff06792c583fa443e72d8b7b3f6a165492a
snipeet03 <navneetlonare77@gmail.com>
2025-11-01 22:34:34 +0530

    feat: implement AddTodo and Todo components with add/remove functionality

--- intermediate/reduxToolkitTodo/src/app/store.js
+++ intermediate/reduxToolkitTodo/src/app/store.js
@@ -1,8 +1,10 @@
 import {configureStore} from '@reduxjs/toolkit';
 import  { addTodo , removeTodo } from '../features/todo/todoSlice';
+import todoReducer from '../features/todo/todoSlice'
 
 
-
-const store = configureStore({});
+const store = configureStore({
+    reducer : todoReducer
+});
 
 
